c7801bef0463b8d3b43736ebc3a7283d0defbd5b
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2024-03-19 12:30:11 -0500

    Primer commit

---
+++ receta/lib/screens/bebida_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class BebidaScreen extends StatelessWidget{
+  const BebidaScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hola bebida");
+  }
+}
