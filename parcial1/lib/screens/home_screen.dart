55a9548e8105bef25a2d2ed900029033a109f119
JaVergelVe <javergelve@ufpso.edu.co>
2024-03-27 13:54:02 -0500

    Iniciando primer parcial

---
+++ parcial1/lib/screens/home_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:parcial1/screens/bebida_screen.dart';
+import 'package:parcial1/screens/receta_screen.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _indicador = 0;
+  static final List _menu = [
+    const RecetaScreen(),
+    const BebidaScreen(),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hola aplicacion"),
+      ),
+      body: _menu[_indicador],
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(5),
+          children: [
+            ListTile(
+              title: const Text("Receta"),
+              onTap: (){
+                _indicador = 0;
+                setState(() {});
+                Navigator.pop(context);
+              },
+              ),
+            ListTile(
+              title: const Text("Bebidas"),
+              onTap: (){
+                _indicador = 1;
+                setState(() {});
+                Navigator.pop(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
